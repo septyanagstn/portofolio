09252046070ec157b25f7788762224d3cdf75444
septyanagstn <septyanadoang@gmail.com>
2025-05-07 05:03:13 +0700

    update resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Button } from "react-bootstrap";
 import Particle from "../Particle";
 
 function ResumeNew() {
@@ -7,6 +7,20 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
+        
+        {/* Tombol Unduh */}
+        <Row style={{ justifyContent: "center", marginBottom: "20px", position: "relative"}}>
+          <Button
+            variant="primary"
+            href="/CV_Septyana-Agustina.pdf"
+            download
+            style={{ maxWidth: "250px" }}
+          >
+            Unduh CV
+          </Button>
+        </Row>
+
+        {/* Tampilan PDF */}
         <Row style={{ justifyContent: "center" }}>
           <iframe
             src="/CV_Septyana-Agustina.pdf"
